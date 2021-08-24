93a0f8411988319ad87bd9308fa46aa3f73ff40f
ravi159357 <ravirathore159357@gmail.com>
2021-08-24 19:19:13 +0530

    wcat commit

---
+++ index.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+
+
+const fs = require("fs");
+const { stringify } = require("querystring");
+ 
+let arguments = process.argv.slice(2);
+
+let flags = [];
+let filenames = [];  
+let secondaryArguments = [];
+
+for(let i of arguments){
+    if(i[0] == "-"){
+        flags.push(i);
+    } else if(i[0] == "$") {
+        secondaryArguments.push(i.slice(1));
+    }else{
+        filenames.push(i);
+    }
+}
+
+// if(flags.length == 0 && filenames.length!=0){
+//     for(let file of filenames){
+//         console.log(fs.readFileSync(file,"utf-8"));
+//     }
+// }else{
+//     for(flag of flags){
+//         if(flag == "-rs"){
+//             for(let file of filenames){
+//                 let fileData = fs.readFileSync(file,"utf-8")
+//                 console.log(fileData.split(" ").join(""));
+//             }
+//         }
+//     }
+// }
+
+for(let file of filenames){
+    let fileData =fs.readFileSync(file,"utf-8");
+    for(let flag of flags){
+        if(flag == "-rs"){
+            fileData = removeAll(fileData," ");
+          // fileData = fileData.split(" ").join("");
+
+        }
+        if(flag == "-rn"){
+            fileData = removeAll(fileData,"\r\n");
+          // fileData = fileData.split("\r\n").join("");
+        }
+        if(flag == "-rsc") {
+            for(let secondaryArgument of secondaryArguments) {
+                fileData = removeAll(fileData,secondaryArgument);
+                //fileData = fileData.split("\r\n").join("");
+            }
+        }
+
+        if(flag == "-rc"){
+            for(let secondaryArgument of secondaryArguments){
+                fileData=fileData.split(secondaryArgument).join("");
+            }    
+        }
+
+        if(flag == "-s"){
+            //fileData = fileData.split("\r\n").join("");
+            fileData = addSequence(fileData);
+        }
+
+        if(flag == "-sn"){
+            //fileData = fileData.split("\r\n").join("");
+            fileData = addSequenceTnel(fileData);
+        }
+
+        if(flag == "-rel"){   
+            removeExtraLines(fileData);
+            // printing in function directly
+        }
+
+        if(flag == "-rmel"){   
+            removeManyExtraLines(fileData);
+            // printing in function directly           
+        }
+
+        
+
+    }
+   // console.log(fileData);
+}
+
+function removeAll(string, removalData) {
+    return string.split(removalData).join("");
+}
+
+function addSequence(content){
+    let contentArr = content.split("\r\n");
+    for(let i=0;i<contentArr.length;i++){
+        contentArr[i] = (i+1) + " " + contentArr[i];
+    }
+    return contentArr;
+}
+function addSequenceTnel(content){
+    let contentArr = content.split("\r\n");
+    let count=1;
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i]!=""){
+            contentArr[i] = (count++) + " " + contentArr[i];
+        }
+    }
+    return contentArr;
+}
+
+function removeExtraLines(content){
+    let contentArr = content.split("\r\n");
+    let ansArr=[];
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i]!=""){
+            ansArr.push(contentArr[i]);
+        }
+    }
+    for(let i=0;i<ansArr.length;i++){
+      //  console.log(ansArr[i]);
+    }
+    
+}
+
+function removeManyExtraLines(content){
+    let contentArr = content.split("\r\n");
+    let data = [];
+    for(let i=1;i<contentArr.length;i++){
+        if(contentArr[i] == "" && contentArr[i-1] == ""){
+            contentArr[i]=null;
+        }
+        if(contentArr[i] == "" && contentArr[i-1] == null){
+            contentArr[i]=null;
+        }
+    }
+    console.log("loop 1 ended");
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i]!=null){
+            data.push(contentArr[i]);
+        }
+    }
+
+    for(let i=0;i<data.length;i++){
+        console.log(data[i]);
+      }
+}
---
+++ rs.js
@@ -0,0 +1,29 @@
+
+const fs = require("fs");
+//const { setFlagsFromString } = require("v8");
+let arguments = process.argv.slice(2); // 0th index-> node ka path 1th index-> current file ka path therefore, we start looking for arguments from the second index
+console.log(arguments);
+//argument me koi flag nahi hona chahiye aur hoga 1 ya more than 1 flags ho sakta hai
+//we need to separate flags from hyphens
+let flags = [];
+let filenames = []; //
+
+//Segregation
+for(let i of arguments)
+{
+    if(i[0] == '-'){
+        flags.push(i);
+    }
+    else{
+        filenames.push(i);
+    }        
+}
+        // //console.log(flags);  -> prints the segregated flags array
+        // //1. wcat file1 file2 -> prints all files one by one
+        // if(flags.length == 0 && filenames.length != 0){ 
+        //     for(let file of filenames){
+        //         console.log(fs.readFileSync(file, "utf-8"));
+        //     }
+        // }
+        // else{   //whenever flags exist, we need to manipulate according to the flag given
+        //     let fileData =
